6c06ea47eac263c3e596d5df1e135c59af32ea86
Cheng Jiahui <1667206199@qq.com>
2023-03-28 17:49:12 +0800

    node

--- mongodb数据库/book/routes/index.js
+++ mongodb数据库/book/routes/index.js
@@ -20,9 +20,17 @@ const AccountModel = require('../models/AccountModel');
 /* 记账本列表 */
 router.get('/account', function (req, res, next) {
     //获取账单信息
-    let accounts = db.get('accounts').value();
-    console.log(accounts);
-    res.render('list', {accounts: accounts});
+    // let accounts = db.get('accounts').value();
+    // console.log(accounts);
+    AccountModel.find().sort({time: -1}).exec((err,data)=>{
+            if (err){
+                console.log('读取失败');
+                return;
+            }
+        // console.log(data)
+        res.render('list', {accounts: data});
+    })
+
 });
 
 //添加记录
@@ -31,25 +39,20 @@ router.get('/account/create', function (req, res, next) {
 });
 //新增记录
 router.post('/account', (req, res) => {
-    //获取请求体数据
-    //  console.log(req.body);
-    //插入数据库
-
+    //查看表单数据
+   // console.log(req.body);
+        //插入数据库
     AccountModel.create({
-        ...req.body,
-    },(err,data)=>{
-        if (err){
-            res.status(500).send('插入失败');
-            return;
-        }
-        console.log(...req.body);
+    ...req.body,
+    },()=>{
         //成功提醒
+        let id =shortid.generate();
+// //写入文件
+        db.get('accounts').unshift({id:id,...req.body}).write();
         res.render('tixing', {mgs: '添加成功··', url: '/account'});
     })
-    //生成id
-    // let id  = shortid.generate()
-    // // //写入文件
-    //  db.get('accounts').unshift({id:id,...req.body}).write();
+
+
 
 })
 //删除
@@ -57,9 +60,14 @@ router.get('/account/:id', function (req, res, next) {
     //获取id
     let id = req.params.id;
     //删除
-    db.get('accounts').remove({id: id}).write();
-    //
-    res.render('tixing', {mgs: '删除成功··', url: '/account'});
+   AccountModel.deleteOne({_id:id},(err)=>{
+       if (err){
+        res.status(500).send('删除失败');
+       }
+       //提醒
+       res.render('tixing', {mgs: '删除成功··', url: '/account'});
+   })
+
 });
 console.log('http://127.0.0.1:3000/account/create')
 module.exports = router;
